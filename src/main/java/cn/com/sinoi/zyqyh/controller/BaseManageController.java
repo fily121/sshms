e4d09fe8467a87869dbf7ef3a90d1b4b9e3b0171
qiangchengxi <qiangchengxi@liandisys.com.cn>
2016-08-26 09:59:12 +0800

    用户管理模块

---
+++ src/main/java/cn/com/sinoi/zyqyh/controller/BaseManageController.java
@@ -0,0 +1,26 @@
+package cn.com.sinoi.zyqyh.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <p>
+ * Title: 基础管理管理控制器
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ *
+ * @author 强成西
+ *
+ * <pre>
+ *         修改记录: 版本号 修改人 修改日期 修改内容
+ */
+@Controller
+@RequestMapping("baseManage")
+public class BaseManageController {
+
+    private static final Logger logger = Logger.getLogger(BaseManageController.class);
+
+}
